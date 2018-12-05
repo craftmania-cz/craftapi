359548a61a00021b78d692670ee2eae2aff5f7f1
MrWakeCZ <joseff.kral@gmail.com>
2018-12-05 20:15:30 +0100

    Úprava config wrapperu

--- src/utils/config-rewrapper.ts
+++ src/utils/config-rewrapper.ts
@@ -1,10 +1,13 @@
 
 export = {
+	app: {
+		port: process.env.PORT,
+		environment: process.env.ENVIRONMENT
+	},
 	mysql: {
 		host: process.env.DB_HOST,
 		user: process.env.DB_USER,
 		password: process.env.DB_PASS,
 		database: process.env.DB_DATABASE,
-	},
-	env: process.env.ENVIRONMENT,
-}
+	}
+};
