e9f0b4bdfe2dcf20cf39b8065ad3761287ac6340
MrWakeCZ <joseff.kral@gmail.com>
2021-07-10 23:51:34 +0200

    McMMO Stats: Nyní používají dynamické cesty `/:server/:abilityType`

--- src/controllers/economy/mcmmo_stats.ts
+++ src/controllers/economy/mcmmo_stats.ts
@@ -4,8 +4,22 @@ import { SQLManager } from "../../managers/SQLManager";
 
 namespace Mcmmo_stats {
 
-	export async function getTop50Swords(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.swords, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.swords DESC LIMIT 50;')
+	export async function getMcMMOStatistics(req: any, res: any) {
+
+		const serverType: string = req.params.server;
+		const abilityType: string = req.params.abilityType;
+
+		if (serverType === null) {
+			Res.property_required(res, "Missing server type in URL.");
+			return;
+		}
+
+		if (abilityType === null) {
+			Res.property_required(res, "Missing McMMO ability type in URL.");
+			return;
+		}
+
+		const data = await SQLManager.knex.raw('SELECT ms.' + abilityType + ' AS value, mu.user, mu.uuid FROM ' + serverType + '.mcmmo_skills ms INNER JOIN ' + serverType + '.mcmmo_users mu ON mu.id = ms.user_id ORDER BY value DESC LIMIT 50;')
 			.on('query-error', (error: any) => {
 				log.error(error);
 				return Res.error(res, error);
@@ -20,327 +34,13 @@ namespace Mcmmo_stats {
 				"index": index,
 				"nick": data.user,
 				"uuid": data.uuid,
-				"value": data.swords
+				"value": data.value
 			});
 			return;
 		});
 		Res.success(res, finalResults);
 		return;
 	}
-
-	export async function getTop50Taming(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.taming, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.taming DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.taming
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Mining(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.mining, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.mining DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.mining
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Woodcutting(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.woodcutting, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.woodcutting DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		console.log(data);
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.woodcutting
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Repair(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.repair, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.repair DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.repair
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Unarmed(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.unarmed, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.unarmed DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.unarmed
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Herbalism(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.herbalism, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.herbalism DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.herbalism
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Excavation(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.excavation, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.excavation DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.excavation
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Archery(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.archery, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.archery DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.archery
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Axes(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.axes, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.axes DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.axes
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Acrobatics(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.acrobatics, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.acrobatics DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.acrobatics
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Fishing(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.fishing, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.fishing DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.fishing
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Alchemy(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.alchemy, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.alchemy DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.alchemy
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
-	export async function getTop50Total(_req: any, res: any) {
-		const data = await SQLManager.knex.raw('SELECT ms.total, mu.user, mu.uuid FROM survival.mcmmo_skills ms INNER JOIN survival.mcmmo_users mu ON mu.id = ms.user_id ORDER BY ms.total DESC LIMIT 50;')
-			.on('query-error', (error: any) => {
-				log.error(error);
-				return Res.error(res, error);
-			});
-		if (!data.length) {
-			return Res.not_found(res);
-		}
-		let finalResults: any = [];
-		data[0].forEach((data: any, index: number) => {
-			index++;
-			finalResults.push({
-				"index": index,
-				"nick": data.user,
-				"uuid": data.uuid,
-				"value": data.total
-			});
-			return;
-		});
-		Res.success(res, finalResults);
-		return;
-	}
-
 }
 
 export default Mcmmo_stats;
--- src/routes/economy/LeaderboardMcmmoRoutes.ts
+++ src/routes/economy/LeaderboardMcmmoRoutes.ts
@@ -11,28 +11,18 @@ export class LeaderboardMcmmoRoutes {
 		this.init();
 	}
 
-	public missingRoute(_req: Request, res: Response, _next: NextFunction) {
+	public missingServerRoute(_req: Request, res: Response, _next: NextFunction) {
 		res.json(Res.property_required(res, 'Server route').json);
 	}
 
+	public missingAbilityRoute(_req: Request, res: Response, _next: NextFunction) {
+		res.json(Res.property_required(res, 'McMMO Ability Type').json);
+	}
+
 	public init() {
-		this.router.get('/', this.missingRoute);
-
-		this.router.get('/swords', Mcmmo_stats.getTop50Swords);
-		this.router.get('/taming', Mcmmo_stats.getTop50Taming);
-		this.router.get('/mining', Mcmmo_stats.getTop50Mining);
-		this.router.get('/woodcutting', Mcmmo_stats.getTop50Woodcutting);
-		this.router.get('/repair', Mcmmo_stats.getTop50Repair);
-		this.router.get('/unarmed', Mcmmo_stats.getTop50Unarmed);
-		this.router.get('/herbalism', Mcmmo_stats.getTop50Herbalism);
-		this.router.get('/excavation', Mcmmo_stats.getTop50Excavation);
-		this.router.get('/archery', Mcmmo_stats.getTop50Archery);
-		this.router.get('/axes', Mcmmo_stats.getTop50Axes);
-		this.router.get('/acrobatics', Mcmmo_stats.getTop50Acrobatics);
-		this.router.get('/fishing', Mcmmo_stats.getTop50Fishing);
-		this.router.get('/alchemy', Mcmmo_stats.getTop50Alchemy);
-		this.router.get('/total', Mcmmo_stats.getTop50Total);
-
+		this.router.get('/', this.missingServerRoute);
+		this.router.get('/:server', this.missingAbilityRoute);
+		this.router.get('/:server/:abilityType', Mcmmo_stats.getMcMMOStatistics);
 	}
 
 }
